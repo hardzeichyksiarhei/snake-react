c2cd83c747ae631d78a53a66917164f0296372a8
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-02-05 22:22:05 +0300

    first commit

---
+++ precache-manifest.1ec0b920f7ea3cdc8fa0a275d1f48cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78316797f623dd64623ec312980edd26",
+    "url": "/snake-react/index.html"
+  },
+  {
+    "revision": "f3587a873b8da011f2da",
+    "url": "/snake-react/static/css/main.19222171.chunk.css"
+  },
+  {
+    "revision": "0823e9e4eeee84e40394",
+    "url": "/snake-react/static/js/2.a3a81e4f.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/snake-react/static/js/2.a3a81e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3587a873b8da011f2da",
+    "url": "/snake-react/static/js/main.b9a3360c.chunk.js"
+  },
+  {
+    "revision": "10f52e0e5b46dd48b0cb",
+    "url": "/snake-react/static/js/runtime-main.6a35adea.js"
+  }
+]);
